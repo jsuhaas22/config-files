134e5d493b03946618375df0bf6e5ad615271fed
Suhaas Joshi <joshiesuhaas0@gmail.com>
2026-02-15 17:13:45 +0530

    emacs: Promote emacs to GUI-/TUI-IDE from editor
    So far, I have been using emacs as a "nicer GUI vim". But since its a lot more
    capable, promote it to a lite IDE by doing the following:
    
    1. Ear-mark some configs only for GUI. In some cases, defaulting some configs to
       the terminal's aesthetics and behaviours is better.
    
    2. Hold a list of required packages, and install these if not already
       installed. This helps quickly setting emacs up on new machines. These
       packages are notmuch, projectile, magit, dts-mode etc.
    
    3. Forbid emacs from changing `init.el` if I change some config during run-time,
       such as `M-x customize-theme`. Instead, direct it to make a different file
       for the same.
    
    Signed-off-by: Suhaas Joshi <joshiesuhaas0@gmail.com>

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Forbid emacs from editing `init.el` directly.
+;; I did `customize-themes` and it edited this file. This file should be edited
+;; only by the user.
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror)
+
 ;; Don't show the splash screen
 (setq inhibit-startup-message t)
 
@@ -14,7 +20,9 @@
 (global-display-line-numbers-mode 1)
 
 ;; load dark theme
-(load-theme 'deeper-blue t)
+(if (display-graphic-p)
+      (load-theme 'deeper-blue t)
+)
 
 ;; set font to intelOne-Mono
 (set-frame-font "intelOne Mono 12" nil t)
@@ -46,9 +54,6 @@
 ;; display line numbers in each buffer
 (global-display-line-numbers-mode 1)
 
-;; load dark theme
-(load-theme 'deeper-blue t)
-
 ;; set font to intelOne-Mono
 (set-frame-font "intelOne Mono 12" nil t)
 
@@ -64,33 +69,19 @@
 ;; maximize frame at startup
 ;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+(when (display-graphic-p)
 (global-set-key (kbd "C-S-f") 'forward-word)
 (global-set-key (kbd "C-S-b") 'backward-word)
 (global-set-key (kbd "C-.") 'xref-find-definitions) ;; Remap M-. (xref-find-definitions) to C-.
 (global-set-key (kbd "C-,") 'xref-pop-marker-stack) ;; Remap M-, (xref-pop-marker-stack) to C-,
 (global-set-key (kbd "C-S-v") 'scroll-down-command)
+)
 
 ;; Turn on auto-fill-mode globally with Linux kernel line length (80)
 (setq-default fill-column 80)
 (add-hook 'text-mode-hook #'auto-fill-mode)       ;; for text-mode and derivatives
 (add-hook 'prog-mode-hook #'auto-fill-mode)       ;; for programming modes
 (add-hook 'conf-mode-hook #'auto-fill-mode)       ;; for config files
-
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dts-mode magit markdown-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (defun modi/revert-all-file-buffers ()
   "Refresh all open file buffers without confirmation.
@@ -114,3 +105,72 @@ will be killed."
             (kill-buffer buf)
             (message "Killed non-existing/unreadable file buffer: %s" filename))))))
   (message "Finished reverting buffers containing unmodified files."))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; package configs
+;; sources:
+;;	https://stackoverflow.com/questions/55038594/setting-up-emacs-on-new-machine-with-init-el-and-package-installation
+;;	https://docs.projectile.mx/projectile/usage.html
+;; download packages if they aren't already downloaded.
+;; this block reduces startup time by a bit but I don't care.
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+;; I may have forgotten adding a few. Might have to be updated on a new machine.
+(setq req-packages
+      '(projectile
+	ag
+	vertico
+	which-key-posframe
+	magit
+	notmuch
+	dts-mode
+	markdown-mode))
+;; Iterate on packages and install missing ones
+(dolist (pkg req-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+;; projectile specific changes start ;;
+;; all of the below in the projectile section is copied from the docs.
+
+;; Optional: ag is nice alternative to using grep with Projectile
+(use-package ag
+  :ensure t)
+
+;; Optional: Enable vertico as the selection framework to use with Projectile
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode +1))
+
+;; Optional: which-key will show you options for partially completed keybindings
+;; It's extremely useful for packages with many keybindings like Projectile.
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode +1))
+
+(use-package projectile
+  :ensure t
+  :init
+  (setq projectile-project-search-path '("~/projects/" ("~/git/" . 1)))
+  :config
+  ;; On Linux, however, I usually go with another one
+  (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+  (global-set-key (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
+;; projectile specific changes start ;;
+
+;; notmuch config specific changes start ;;
+(require 'notmuch)
+(setq notmuch-saved-searches
+      '((:name "Inbox"   :query "tag:inbox" :key "i")
+        (:name "Unread"  :query "tag:unread" :key "u")
+        (:name "linux-crypto"  :query "folder:linux-crypto" :key "c")
+        (:name "u-boot"  :query "folder:u-boot" :key "b")
+        (:name "linux-security-modules" :query "folder:linux-security-modules" :key "s")))
+;; notmuch config specific changes start ;;
