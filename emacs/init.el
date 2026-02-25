5e2e1b2664cdac1267f022ddaeb371e59ea4bf66
Suhaas Joshi <joshiesuhaas0@gmail.com>
2026-02-25 19:15:04 +0530

    emacs: create a new directory and remove repetitions
    Lots of repititons in the init.el file, so remove them. Also create a emacs/
    directory to hold all emacs files, since there will be more than one.
    
    Signed-off-by: Suhaas Joshi <joshiesuhaas0@gmail.com>

--- emacs/init.el
+++ emacs/init.el
@@ -19,41 +19,6 @@
 ;; display line numbers in each buffer
 (global-display-line-numbers-mode 1)
 
-;; load dark theme
-(if (display-graphic-p)
-      (load-theme 'deeper-blue t)
-)
-
-;; set font to intelOne-Mono
-(set-frame-font "intelOne Mono 12" nil t)
-
-;; make it truly full-screen
-(setq frame-resize-pixelwise t)
-
-;; set tab
-(setq-default c-basic-offset 8)
-
-;; enable electric pair mode
-(electric-pair-mode -1)
-
-;; maximize frame at startup
-;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-;; Don't show the splash screen
-(setq inhibit-startup-message t)
-
-;; disable beep and flash
-(setq visible-bell nil)
-(setq ring-bell-function 'ignore)
-
-;; get rid of menu-, scroll- and tool-bars
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-;; display line numbers in each buffer
-(global-display-line-numbers-mode 1)
-
 ;; set font to intelOne-Mono
 (set-frame-font "intelOne Mono 12" nil t)
 
